d6000dd2c21676142b7305c68e12e75006093c4c
jannahsherif <144076374+jannahsherif@users.noreply.github.com>
2024-07-29 13:39:46 +0300

    Update website_2.py

--- website_2.py
+++ website_2.py
@@ -1,1 +1,28 @@
-edit 
+import streamlit as st
+import webbrowser
+
+st.set_page_config(page_title="Jannah Webpage", layout="wide")
+
+st.subheader("Welcome to Jannah's Web")
+st.title("Demo website")
+st.write("This is for integration project")
+
+with st.container():
+    st.write("---")
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.header("link to sherkety")
+        st.write("###")
+        st.write("[Sherkety >](https://sherkety.net/)")
+
+# Login Section
+st.write("---")
+st.header("Login to Facebook")
+username = st.text_input("Username")
+password = st.text_input("Password", type="password")
+
+if st.button("Login"):
+    # Redirect to Facebook
+    # Display username and password
+    st.write(f"Username: {username}")
+    st.write(f"Password: {password}")
